9e296cb4c94c9ee00045a6feeeaa42e2c2f142f5
Sathiyavani S <sathiyavanisaravanan@gmail.com>
2023-05-31 16:24:11 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,23 @@
+import YTLogo from '../Images/youtube_logo_icon_167938.png';
+import Alarm from '../Images/alarm_alert_bell_notification_ring_icon_123294.png';
+import VideoIcon from '..//Images/videocamera_5631.png';
+import '../Styles/Navbar.css';
 
 const Navbar = () => {
     return (
-        <div className="navbar"></div>
+        <div className="navbar">
+            <div className="logo">
+                <img src={YTLogo} alt="" width={100}/>
+            </div>
+            <div className="searchBar">
+                <input type="text" name="" id="" placeholder="Search" />
+            </div>
+            <div className="userOptions">
+                <img src={VideoIcon} alt="" width={35} height={35}/>
+                <img src={Alarm} alt="" width={35} height={35} />
+                <p>S</p>
+            </div>
+        </div>
     );
 }
 export default Navbar;
